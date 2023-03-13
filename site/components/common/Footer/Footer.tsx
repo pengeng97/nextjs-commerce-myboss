146819d898a7d2f7e477421408dccab04787bb47
Peng Eng Tan <pengeng97@gmail.com>
2023-03-13 16:01:39 +0800

    modified:   site/components/common/Footer/Footer.tsx 	modified:   site/components/ui/Logo/Logo.tsx 	modified:   site/public/site.webmanifest

--- site/components/common/Footer/Footer.tsx
+++ site/components/common/Footer/Footer.tsx
@@ -39,7 +39,7 @@ const Footer: FC<Props> = ({ className, pages }) => {
               <span className="rounded-full border border-accent-6 mr-2">
                 <Logo />
               </span>
-              <span>ACME</span>
+              <span>MYBOSSWHEELS</span>
             </Link>
           </div>
           <div className="col-span-1 lg:col-span-7">
@@ -59,35 +59,13 @@ const Footer: FC<Props> = ({ className, pages }) => {
           <div className="col-span-1 lg:col-span-3 flex items-start lg:justify-end text-primary">
             <div className="flex space-x-4 items-center h-10">
               <ThemeSwitcher />
-              <I18nWidget />
-              <a
-                className={s.link}
-                aria-label="Github Repository"
-                href="https://github.com/vercel/commerce"
-              >
-                <Github />
-              </a>
+
             </div>
           </div>
         </div>
         <div className="pt-6 pb-10 flex flex-col md:flex-row justify-between items-center space-y-4 text-accent-6 text-sm">
           <div>
-            <span>&copy; 2020 ACME, Inc. All rights reserved.</span>
-          </div>
-          <div className="flex items-center text-primary text-sm">
-            <span className="text-primary">Created by</span>
-            <a
-              rel="noopener noreferrer"
-              href="https://vercel.com"
-              aria-label="Vercel.com Link"
-              target="_blank"
-              className="text-primary"
-            >
-              <Vercel
-                className="inline-block h-6 ml-3 text-primary"
-                alt="Vercel.com Logo"
-              />
-            </a>
+            <span>&copy; 2023 MYBOSSWHEELS All rights reserved.</span>
           </div>
         </div>
       </Container>
